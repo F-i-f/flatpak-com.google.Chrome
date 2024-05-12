86b3860ddd40767274c4f6182e50b267642c8c66
Philippe Troin <phil+github-commits@fifi.org>
2024-05-11 20:11:28 -0700

    Embed extra data download.
    This makes the flatpak offline installable.

--- apply_extra.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-ar x chrome.deb data.tar.xz
-rm chrome.deb
-tar -xf data.tar.xz --strip-components=4 ./opt/google/chrome
-rm data.tar.xz nacl*
-cp /app/bin/stub_sandbox chrome-sandbox
